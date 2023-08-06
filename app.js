da61606aec0d5bb4c32bf8675519ab08c202ddac
vpq-is-me <vpq@list.ru>
2023-08-06 21:12:51 +1000

    Add trend first time

--- app.js
+++ app.js
@@ -20,26 +20,30 @@ app.get('/jfile', (req,res)=>{
     res.json( rec_buf);
 })
 
-app.get('/reset_alarm',(req,res)=>{
-    //if(req.body.button==="Reset"){
-	unix_client.write(JSON.stringify({'tag':TAG_CMDRESETALARM}));
-    //}
-     res.end('reset_done');
+app.get('/reset_alarm', (req, res) => {
+    unix_client.write(JSON.stringify({ 'tag': TAG_CMDRESETALARM }));
+    res.end('reset_done');
 })
-//****************** */
-let trend_resp;
-let waiting=0;
-app.post('/basic_trend_data',(req,res)=>{
-    let body=req.body;
-    console.log("receive data->"+JSON.stringify(body));
-    let down_req=Object.assign({tag:3},body);
-    unix_client.write(JSON.stringify(down_req));
-    trend_resp=res;
-    waiting=1;
-})
-async function GetTrendDataFromManager(){
-
+//************************************* */
+function ManagerResponser(){
+    this.query_res_arr=[];
+    this.NewReq=function(req,res){
+        this.query_res_arr.push(res);
+        unix_client.write(JSON.stringify(req));
+    }
+    this.Answer=function(payload){
+        var res=this.query_res_arr.shift();
+        if(typeof(res)!=="undefined"){
+            res.send(JSON.stringify(payload));
+        }
+    }
 }
+var mng_resp=new ManagerResponser();
+//************************************** */
+app.post('/basic_trend_data', (req, res) => {
+    let down_req = Object.assign({ tag: 3 }, req.body);
+    mng_resp.NewReq(down_req,res);
+})
 //****************** */
 app.listen(8080,()=>{
     console.log('server started on port 8080')
@@ -54,22 +58,21 @@ const TAG_REQSNAP        =1;
 const TAG_CMDRESETALARM  =2;
 const TAG_DB_REQUEST     =3;
 
-unix_client.on('connect',() => {
+unix_client.on('connect', () => {
     console.log('Unix socket connected');
-    setInterval(()=>{
-        unix_client.write(JSON.stringify({'tag':TAG_REQSNAP}));
-    },1000);
+    setInterval(() => {
+        unix_client.write(JSON.stringify({ 'tag': TAG_REQSNAP }));
+    }, 1000);
 })
 unix_client.on('data', (msg) => {
     var jrec = JSON.parse(msg.toString());
     //TODO vpq !!! parse can throw exception!!!
     console.log(jrec);
-    if('tag' in jrec){       
-        if(jrec.tag===3 && waiting===1){
-            waiting=0;
-            trend_resp.send(JSON.stringify(jrec));
+    if ('tag' in jrec) {
+        if (jrec.tag === 3) {
+            mng_resp.Answer(jrec);
         }
-    }else{
-         rec_buf = jrec;
-    }    
+    } else {
+        rec_buf = jrec;
+    }
 })
--- src/basic_trend.js
+++ src/basic_trend.js
@@ -2,8 +2,8 @@ async function GetTrendData(){
     let query={
         base:"last",
         direction:"up",
-        amount:5,
-        columns:["id","date","WF_counter"]
+        amount:50,
+        columns:["id","PP_capacity_avg","PP_capacity","TK_volume","PP_RUN_MIN"]
     };
     try {
         const resp = await fetch('/basic_trend_data',{
@@ -13,8 +13,62 @@ async function GetTrendData(){
             },
             body: JSON.stringify(query),
          });
-        let json = await resp.json();
-        console.log(json);  
+        let ch_json = await resp.json();
+
+        new Chart("basic_trend", {
+            type: "line",
+            data: {
+                labels: ch_json.data.id,
+                datasets: [{
+                    label: "PP_capacity_avg",
+                    backgroundColor: "rgba(0,0,255,1.0)",
+                    borderColor: "rgba(0,0,255,0.8)",
+                    data: ch_json.data.PP_capacity_avg,
+                    yAxisID: 'y',
+                },
+                {
+                    label: "PP_capacity",
+                    backgroundColor: "rgba(255,0,155,1.0)",
+                    borderColor: "rgba(255,0,155,0.8)",
+                    data: ch_json.data.PP_capacity,
+                    yAxisID: 'y',
+                },
+                {
+                    label: "TK_volume",
+                    backgroundColor: "rgba(0,255,0,1.0)",
+                    borderColor: "rgba(0,255,0,0.8)",
+                    data: ch_json.data.TK_volume,
+                    yAxisID: 'y1',
+                },
+                {
+                    label: "PP_RUN_MIN",
+                    backgroundColor: "rgba(100,0,200,1.0)",
+                    borderColor: "rgba(100,0,200,0.8)",
+                    data: ch_json.data.PP_RUN_MIN,
+                    yAxisID: 'y1',
+                }
+                ]
+            },
+            //bezierCurve: false,
+
+            options: {
+                scales:{
+                    x:{
+                        title:{
+                            color:'red',
+                            display: true,
+                            text: "basic pump trend",
+                        }
+                    }
+                }
+                // animation: true,
+                // tension: 0.4,
+                // animationEasing: "easeInBounce",
+                // bezierCurve:true,
+            }
+        });
+
+        console.log(ch_json);  
     }catch(err){
         console.error("Trend data request error->", err);
     }
@@ -22,21 +76,25 @@ async function GetTrendData(){
 }
 GetTrendData();
 
-const xValues = [50,60,70,80,90,100,110,120,130,140,150];
-const yValues = [7,8,8,9,9,9,10,11,14,14,15];
+// const xValues = [50,60,70,80,90,100,110,120,130,140,150];
+// const yValues = [7,8,8,9,9,9,10,11,14,14,15];
 
-new Chart("basic_trend", {
-  type: "line",  
-  data: {
-    labels: xValues,
-    datasets: [{
-      backgroundColor:"rgba(0,0,255,1.0)",
-      borderColor: "rgba(0,0,255,0.8)",
-      data: yValues
-    }]
-  },
- // options:{...}
-});
+// new Chart("basic_trend", {
+//   type: "line",  
+//   data: {
+//     labels: xValues,
+//     datasets: [{
+//       backgroundColor:"rgba(0,0,255,1.0)",
+//       borderColor: "rgba(0,0,255,0.8)",
+//       data: yValues
+//     }]
+//   },
+//  // options:{...}
+// });
+
+
+
+
 // (async function() {
 //     const data = [
 //       { year: 2010, count: 10 },
