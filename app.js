5521e0bf2d05163cbd43955448ebc81f257bbd07
vpq-is-me <vpq@list.ru>
2023-10-21 14:18:40 +0200

    add connection to septic manager and blind dummy alarms

--- app.js
+++ app.js
@@ -58,8 +58,8 @@ app.listen(8080,()=>{
 });
 //************************************************************** */
 const net=require('net');
-const Stream = require("stream");
 const UP2WEB_SOCKET="/tmp/water_pump_socket.socket";
+const SEPTIC_UP2WEB_SOCKET="/tmp/septic_socket.socket";
 
 var unix_client=net.createConnection(UP2WEB_SOCKET);
 const TAG_REQSNAP        =1;
@@ -89,6 +89,7 @@ unix_client.on('data', (msg) => {
     }
 })
 //************************************************************** */
+const sept_tout_mask=(1 << 10);
 let alarm_bitfields={
     /* WP_ALARM_CHECKVALVE_LEAK */      [1 << 2]:"ВНИМАНИЕ: Проверь обратный клапан",
     /* WP_ALARM_PUMP_CAPACITY_LOW */    [1 << 3]:"ВНИМАНИЕ: Снижена производительность насоса",
@@ -106,6 +107,14 @@ function TGAlarmDispather(){
     this.Update=function(new_alarm_state){
 //   cnt++
 //   if(cnt>=2 && cnt<=4)new_alarm_state=16
+        /***XX add a 'blind' against 'septic BLE message timeout' alarm. For some reason in water pump there is
+         bug that gives above mentioned alarm even septic work properly and send it's message in predifined maner*/
+        if(septic_message_tout>=SEPTIC_MAX_TIMEOUT_FROM_LAST_MSG_sec){
+            new_alarm_state=new_alarm_state | sept_tout_mask; 
+        }else{
+            new_alarm_state=new_alarm_state & (~sept_tout_mask);
+        }
+        /***END XX... */
         if(this.prev_alarm_state!==new_alarm_state){
             let diff=this.prev_alarm_state ^ new_alarm_state;
             for(key in alarm_bitfields){
@@ -126,5 +135,41 @@ function TGAlarmDispather(){
 }
 
 tg_alarm_disp=new TGAlarmDispather();
-///TODO add telegramm as alarm receiver !!!!
+var septic_message_tout;
+const SEPTIC_MAX_TIMEOUT_FROM_LAST_MSG_sec=(5*60);
+(function () {
+    var con_established=0;
+    var sept_unix_client;
+    setInterval(() => {
+        if(con_established===0){
+            console.log("Try to connect to Septic");
+            sept_unix_client = net.createConnection(SEPTIC_UP2WEB_SOCKET);
+            sept_unix_client.on("error", (err) => {
+                console.log(err);
+                con_established=0;
+                sept_unix_client=undefined;
+            })
+            sept_unix_client.on('connect',()=>{
+                console.log("septic is now connected");
+                con_established=1;
+            })
+            sept_unix_client.on('end',()=>{
+                console.log("septic accidantly disconnected");
+                con_established=0;
+                sept_unix_client=undefined;
+            })
+            sept_unix_client.on('data',(data)=>{
+                //console.log(data.toString());
+                var sept_ans = JSON.parse(data.toString());                
+                if('msg_timer' in sept_ans){
+                    septic_message_tout=sept_ans.msg_timer;
+                    console.log(`Septic message timeout ${septic_message_tout}`);
+                }                
+            })
+        }else{
+            sept_unix_client.write(JSON.stringify({ 'tag': TAG_REQSNAP, 'val': 'msg_timer' }));   
+        }
 
+    }, 3000);
+
+}());
